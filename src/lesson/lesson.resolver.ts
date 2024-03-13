61e746a06dee1ecb2cf163fb495c9690e2b75aad
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-03-13 22:59:28 +0200

    feat(lesson):Lesson resolver

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,15 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+	@Query((returns) => LessonType)
+	lessons(): LessonType {
+		return {
+			id: '123',
+			name: 'bio 101',
+			startDate: new Date().toISOString(),
+			endDate: new Date().toISOString(),
+		};
+	}
+}
